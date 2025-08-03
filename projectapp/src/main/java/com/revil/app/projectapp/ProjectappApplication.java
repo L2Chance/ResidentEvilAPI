8cd48983d8351f66d7951ee38655d48fa2049c7d
L2Chance <Estudiandoyavanzando@gmail.com>
2025-08-03 18:35:15 -0300

    Agregada configuración de base de datos

--- projectapp/src/main/java/com/revil/app/projectapp/ProjectappApplication.java
+++ projectapp/src/main/java/com/revil/app/projectapp/ProjectappApplication.java
@@ -1,5 +1,6 @@
 package com.revil.app.projectapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectappApplication {
 
 	public static void main(String[] args) {
+		// Carga las variables del archivo .env
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(ProjectappApplication.class, args);
 	}
 
